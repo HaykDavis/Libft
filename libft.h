ed10bcd9f51bc8c92ea891f11eed9e1de0f1de70
Кирилл Зайченко <kirillzajcenko@Kirills-MacBook-Pro.local>
2021-04-24 10:46:48 +0300

    second commit

---
+++ libft.h
@@ -0,0 +1,11 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+void	ft_bzero(void *s, size_t n);
+char	*ft_memset(void *m, int s, size_t l);
+
+#endif
